1149de97c403f3db629e4edd68903730e5e3c107
彭佳勋 <pengjx@mingyuanyun.com>
2018-02-13 10:17:25 +0800

    完善代码

--- src/router/index.js
+++ src/router/index.js
@@ -1,1 +1,13 @@
+import Vue from 'vue'
+import Router from 'vue-router'
 const Index = () => import('views/index')
+
+Vue.use(Router)
+
+export default new Router({
+    routes: [{
+        path: '/',
+        name: 'Index',
+        component: Index
+    }]
+})

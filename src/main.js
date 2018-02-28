1d87403b6fbb33d2fa81326a1eeed1e3efc50045
彭佳勋 <pengjx@mingyuanyun.com>
2018-02-28 09:45:26 +0800

    增加elementui引用，修改文件夹结构

--- src/main.js
+++ src/main.js
@@ -1,3 +1,23 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
 import 'normalize.css'
 import './assets/css/index.scss'
 import store from './store'
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+})
